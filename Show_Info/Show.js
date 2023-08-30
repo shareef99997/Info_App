7f8b89e07c93411f1b95c9ee919d8588241c0f92
Your Nameemail <shareef.99997@gmail.com>
2023-08-30 22:05:17 +0300

    Added weather app

---
+++ Show_Info/Show.js
@@ -0,0 +1,64 @@
+let ShowNameRef = document.getElementById("Show-name");
+let searchBtn = document.getElementById("search-btn");
+let result = document.getElementById("result");
+
+//function to fetch data from api
+
+let getShow = () => {
+    let ShowName = ShowNameRef.value;
+    let url = `https://www.omdbapi.com/?t=${ShowName}&apikey=${key}`;
+    //if input field is empty
+
+    if (ShowName.length <= 0) {
+        result.innerHTML = `<h3 class="msg">Please enter a Show or tv-show name </h3>`;
+    }
+
+    //if input isn't empty 
+    else {
+        fetch(url).then((resp) => resp.json()).then((data) => {
+            
+            //if Show exist in database
+            if (data.Response == "True") {
+                result.innerHTML = `
+                    <div class="info">
+                        <img src=${data.Poster} class="poster">
+                        <div>
+                            <h2>${data.Title}</h2>
+                            <div class="rating">
+                                <img src="star-icon.svg">
+                                <h4>${data.imdbRating}</h4>
+                            </div>
+                            <div class="details">
+                                <span>${data.Rated}</span>
+                                <span>${data.Year}</span>
+                                <span>${data.Runtime}</span>
+                            </div>
+                            <div class="genre">
+                                <div>${data.Genre.split(",").join("</div><div>")}</div>
+                            </div>
+                            <div class="Language">
+                            <div>${data.Language}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <h3>Plot:</h3>
+                    <p>${data.Plot}</p>
+                    <h3>Cast:</h3>
+                    <p>${data.Actors}</p>
+                `;
+            }
+
+            //if Show doesn't exist in database
+            else {
+                result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+            }
+        })
+            //if error occurs
+            .catch(() => {
+                result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
+            });
+    }
+};
+
+searchBtn.addEventListener("click", getShow);
+window.addEventListener("load", getShow);
---
+++ Weather/key.js
@@ -0,0 +1,3 @@
+//Enter api key that recieved on your email here
+
+key = "c8332484d9f63985a936e7da87215eb0";
