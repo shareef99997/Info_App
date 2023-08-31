5edf3119dc87cceef9972f714953f9a2855b147a
Your Nameemail <shareef.99997@gmail.com>
2023-08-31 18:07:20 +0300

    Finished

---
+++ index.js
@@ -0,0 +1,13 @@
+function Show_Info() {
+  window.location.href = "Show_Info/Show.html";
+}
+
+function Weather() {
+  window.location.href = "Weather/Weather.html";
+}
+function Currency() {
+  window.location.href = "Currency/Currency.html";
+}
+function Country() {
+  window.location.href = "Country Guide App/Country.html";
+}
